637c00100af05dac89f346e9bb6bdaa978fa37ae
Daniel Martin <daniel.t.martin@protonmail.com>
2025-03-14 11:33:20 -0400

    zero memory and handle echo RAM

--- src/MMU.zig
+++ src/MMU.zig
@@ -18,15 +18,19 @@ const scroll_x = 0xFF43;
 const rom_bank_number = 0x2000;
 
 const cartridge_end = 0x7FFF;
-
 const vram_start = 0x8000;
 const vram_end = 0x9FFF;
+const wram_start = 0xC000;
+const echo_ram_start = 0xE000;
+const echo_ram_end = 0xFDFF;
+const io_registers_start = 0xFF00;
+const io_registers_end = 0xFF7F;
 
 alloc: Allocator,
 ppu: *PPU,
 boot_rom: ?[]u8 = null,
 boot_rom_mapped: bool = false,
-memory: [65536]u8 = undefined,
+memory: [65536]u8 = [_]u8{0} ** 65536,
 cartridge: *Cartridge,
 fix_y_coordinate: bool = false,
 
@@ -78,6 +82,8 @@ pub fn writeMem(self: *MMU, pointer: u16, val: u8) void {
         else => {
             if (pointer >= vram_start and pointer <= vram_end) {
                 self.ppu.vram[pointer - vram_start] = val;
+            } else if (pointer >= echo_ram_start and pointer <= echo_ram_end) {
+                return;
             } else {
                 self.memory[pointer] = val;
             }
@@ -106,6 +112,12 @@ pub fn readMem(self: *MMU, pointer: u16) u8 {
     }
     if (pointer >= vram_start and pointer <= vram_end) {
         return self.ppu.vram[pointer - vram_start];
+    }
+    if (pointer >= io_registers_start and pointer <= io_registers_end) {
+        return 0xFF;
+    }
+    if (pointer >= echo_ram_start and pointer <= echo_ram_end) {
+        return self.memory[wram_start + (pointer - echo_ram_start)];
     }
     return self.memory[pointer];
 }
